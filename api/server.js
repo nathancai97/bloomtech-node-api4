24ace01bd2f0ef05e698f87a0843da4965e0aa89
Nathan Cai <wenjuncai97@gmail.com>
2022-05-12 22:08:36 -0400

    add stuff

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const fortunes = require('./fortunes')
+const Stuff = require('./stuff-model')
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('<h1>HEROKU DEPLOYMENT!!</h1>')
@@ -14,5 +17,33 @@ server.get('/fortune', (req, res) => {
     let randomIndex = Math.floor(Math.random() * fortunes.length);
     res.send(`<h1>${fortunes[randomIndex]}</h1>`)
 })
+
+
+server.get('/stuff', (req, res) => {
+    Stuff.getAll()
+        .then(result => {
+            res.json(result)            
+        })
+        .catch()
+})
+
+server.get('/stuff:index', (req, res) => {
+    Stuff.getByIndex(req.params.index)
+        .then(result => {
+            if(result == null) {
+                res.status(404).json({message: 'not found'})
+                return;
+            }
+            res.json(result)            
+        })
+})
+
+server.post('/stuff', (req, res) => {
+    Stuff.add(req.body)
+        .then(result => {
+            res.status(201).json(result)            
+        })
+})
+
 
 module.exports = server;
